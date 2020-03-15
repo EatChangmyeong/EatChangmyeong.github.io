f04b9eb98883b4f114097f460dc963407e78478c
EatChangmyeong <dlaud5379@naver.com>
2020-03-15 22:45:17 +0900

    Rewrite TOC postprocessor

--- assets/js/postprocess.js
+++ assets/js/postprocess.js
@@ -12,11 +12,49 @@ document.addEventListener('DOMContentLoaded', function() {
 		return Math.min(a, b);
 	});
 
-	hlist.forEach(function(x) {
-		x.level -= minLevel;
-	});
+	var divGen = function(list) {
+		var internal = function(depth, shift) {
+			var
+				here = x, to,
+				minLvl = Infinity, level = list[here].level, r = '',
+				isLeaf = level == shift;
+			
+			console.log(depth, shift);
 
-	$('.toc').innerHTML = hlist.map(function(x) {
-		return '<div class="toc-' + x.level + '"><a href="#' + x.tag.id + '">' + x.tag.textContent + '</a></div>'
-	}).join('');
+			r += '<div class="toc-node">';
+
+			if(isLeaf) {
+				r += '<a class="toc-leaf" href="#' + list[x].tag.id + '">';
+					r += list[x].tag.textContent;
+				r += '</a>';
+				x++;
+			}
+			to = x;
+
+			while(to < l && list[to].level - shift > 0) {
+				minLvl = Math.min(minLvl, list[to].level);
+				to++;
+			}
+			
+			while(x < to)
+				r += internal(depth + 1, minLvl);
+			
+			r += '</div>';
+			return r;
+		}, l = list.length, x = 0, r = '';
+
+		while(x < l)
+			r += internal(0, minLevel);
+		return r;
+	};
+
+	// hlist.forEach(function(x) {
+	// 	x.level -= minLevel;
+	// });
+
+	// $('.toc').innerHTML = hlist.map(function(x) {
+	// 	return '<div class="toc-' + x.level + '"><a href="#' + x.tag.id + '">' + x.tag.textContent + '</a></div>'
+	// }).join('');
+
+	$('.toc').innerHTML = divGen(hlist);
 });
