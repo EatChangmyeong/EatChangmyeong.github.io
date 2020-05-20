4c3bfb2f47c6944080d3cf5b774f726197f286a1
EatChangmyeong <dlaud5379@naver.com>
2020-05-21 02:20:45 +0900

    Improve script readability

--- assets/js/postprocess.js
+++ assets/js/postprocess.js
@@ -1,62 +1,63 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var $ = document.querySelector.bind(document), $s = document.querySelectorAll.bind(document);
+(function() {
 
-	var hlist = [].map.call($s('main h1, main h2, main h3, main h4, main h5, main h6'), function(x) {
-		return {
-			tag: x,
-			level: +x.tagName[1]
-		};
-	}), minLevel = hlist.map(function(x) {
-		return x.level;
-	}).reduce(function(a, b) {
-		return Math.min(a, b);
-	});
+var $ = document.querySelector.bind(document), $s = document.querySelectorAll.bind(document);
 
-	var divGen = function(list) {
-		var internal = function(depth, shift) {
+function tableOfContents() {
+	var
+		headingNodes = []
+			.map.call($s('main h1, main h2, main h3, main h4, main h5, main h6'),
+				function(x) { return {tag: x, level: +x.tagName[1]}; }),
+		globalMinLevel = headingNodes
+			.map(function(x) { return x.level; })
+			.reduce(function(a, b) { return Math.min(a, b); });
+
+	var generateToc = function(nodeList) {
+		var generateToc_internal = function(depth, shift) {
 			var
-				here = x, to,
-				minLvl = Infinity, level = list[here].level, r = '',
+				here = scanPos, to,
+				minLevel = Infinity, level = nodeList[here].level,
+				result = '',
 				isLeaf = level == shift;
 
-			r += '<li>';
+			result += '<li>';
 
 			if(isLeaf) {
-				r += '<a class="toc-leaf" href="#' + list[x].tag.id + '">';
-					r += list[x].tag.textContent;
-				r += '</a>';
-				x++;
+				result +=
+					'<a class="toc-leaf" href="#' + nodeList[scanPos].tag.id + '">' +
+						nodeList[scanPos].tag.textContent +
+					'</a>';
+				scanPos++;
 			}
-			to = x;
+			to = scanPos;
 
-			while(to < l && list[to].level - shift > 0) {
-				minLvl = Math.min(minLvl, list[to].level);
+			while(to < length && nodeList[to].level - shift > 0) {
+				minLevel = Math.min(minLevel, nodeList[to].level);
 				to++;
 			}
 			
-			if(x < to) {
-				r += '<ul>';
-				while(x < to)
-					r += internal(depth + 1, minLvl);
-				r += '</ul>';
+			if(scanPos < to) {
+				result += '<ul>';
+				while(scanPos < to)
+					result += generateToc_internal(depth + 1, minLevel);
+				result += '</ul>';
 			}
 
-			r += '</li>';
-			return r;
-		}, l = list.length, x = 0, r = '';
+			result += '</li>';
+			return result;
+		};
+		
+		var length = nodeList.length, scanPos = 0, result = '';
 
-		while(x < l)
-			r += internal(0, minLevel);
-		return '<ul>' + r + '</ul>';
+		while(scanPos < length)
+			result += generateToc_internal(0, globalMinLevel);
+		return '<ul>' + result + '</ul>';
 	};
 
-	// hlist.forEach(function(x) {
-	// 	x.level -= minLevel;
-	// });
+	$('.toc').innerHTML = generateToc(headingNodes);
+}
 
-	// $('.toc').innerHTML = hlist.map(function(x) {
-	// 	return '<div class="toc-' + x.level + '"><a href="#' + x.tag.id + '">' + x.tag.textContent + '</a></div>'
-	// }).join('');
-
-	$('.toc').innerHTML = divGen(hlist);
+document.addEventListener('DOMContentLoaded', function() {
+	tableOfContents();
 });
+
+})();
