5238ede331ccb501e8474796fecd835d372b2887
EatChangmyeong <dlaud5379@naver.com>
2021-05-16 02:35:19 +0900

    New stylesheet

--- assets/js/postprocess.js
+++ assets/js/postprocess.js
@@ -1,105 +1,154 @@
-(function() {
+{
 
-var $ = document.querySelector.bind(document), $s = document.querySelectorAll.bind(document);
+const
+	$ = q => document.querySelector(q),
+	$s = q => [...document.querySelectorAll(q)];
 
-var fnlist = {}, required = [];
-
-function noop() {}
-
-fnlist.tableOfContents = function() {
-	var
-		headingNodes = []
-			.map.call($s('main h1, main h2, main h3, main h4, main h5, main h6'),
-				function(x) { return {tag: x, level: +x.tagName[1]}; }),
-		globalMinLevel = headingNodes
-			.map(function(x) { return x.level; })
-			.reduce(function(a, b) { return Math.min(a, b); });
-
-	var generateToc = function(nodeList) {
-		var generateToc_internal = function(depth, shift) {
-			var
-				here = scanPos, to,
-				minLevel = Infinity, level = nodeList[here].level,
-				result = '',
-				isLeaf = level == shift;
-
-			result += '<li>';
-
-			if(isLeaf) {
-				result +=
-					'<a class="toc-leaf" href="#' + nodeList[scanPos].tag.id + '">' +
-						nodeList[scanPos].tag.textContent +
-					'</a>';
-				scanPos++;
+const
+	features = {
+		table_of_contents() {
+			function split_by_level(nodes, level) {
+				const split_list = [];
+				for(const x of nodes) {
+					if(!split_list.length || x.level <= level)
+						split_list.push([]);
+					split_list[split_list.length - 1].push(x);
+				}
+				return split_list;
 			}
-			to = scanPos;
-
-			while(to < length && nodeList[to].level - shift > 0) {
-				minLevel = Math.min(minLevel, nodeList[to].level);
-				to++;
+			function toc_leaf(node) {
+				const a = document.createElement('a');
+				a.className = 'toc-leaf';
+				a.href = `#${node.tag.id}`;
+				a.textContent = node.tag.textContent;
+				return a;
 			}
-			
-			if(scanPos < to) {
-				result += '<ol>';
-				while(scanPos < to)
-					result += generateToc_internal(depth + 1, minLevel);
-				result += '</ol>';
+			function toc_from(nodes, level = 1) {
+				const split = split_by_level(nodes, level);
+				if(split.length === 1 && split[0][0].level !== level)
+					return toc_from(split[0], level + 1);
+				
+				const ol = document.createElement('ol');
+				split.forEach(x => {
+					const li = document.createElement('li');
+					if(x[0].level === level) {
+						li.appendChild(toc_leaf(x[0]));
+						x = x.slice(1);
+					}
+					if(x.length)
+						li.appendChild(toc_from(x, level + 1));
+					ol.appendChild(li);
+				});
+				return ol;
 			}
 
-			result += '</li>';
-			return result;
-		};
-		
-		var length = nodeList.length, scanPos = 0, result = '';
+			$('.toc').appendChild(
+				toc_from(
+					$s(
+						[...Array(6)]
+							.map((_, i) => `main h${i + 1}`)
+							.join(', ')
+					)
+						.map(tag => ({ tag, level: +tag.tagName[1] }))
+				)
+			);
+		},
+		table_of_contents_new() {
+			function split_by_level(nodes, level) {
+				const split_list = [];
+				for(const x of nodes) {
+					if(!split_list.length || x.level <= level)
+						split_list.push([]);
+					split_list[split_list.length - 1].push(x);
+				}
+				return split_list;
+			}
+			function toc_leaf(node) {
+				const a = document.createElement('a');
+				a.className = 'toc-leaf';
+				a.href = `#${node.tag.id}`;
+				a.textContent = node.tag.textContent;
+				return a;
+			}
+			function toc_from(nodes, level = 1) {
+				const split = split_by_level(nodes, level);
+				if(split.length === 1 && split[0][0].level !== level)
+					return toc_from(split[0], level + 1);
+				
+				const ol = document.createElement('ol');
+				split.forEach(x => {
+					const li = document.createElement('li');
+					if(x[0].level === level) {
+						li.appendChild(toc_leaf(x[0]));
+						x = x.slice(1);
+					}
+					if(x.length)
+						li.appendChild(toc_from(x, level + 1));
+					ol.appendChild(li);
+				});
+				return ol;
+			}
 
-		while(scanPos < length)
-			result += generateToc_internal(0, globalMinLevel);
-		return '<ol>' + result + '</ol>';
-	};
+			$('.toc').appendChild(
+				toc_from(
+					$s(
+						[...Array(6)]
+							.map((_, i) => `article h${i + 1}`)
+							.join(', ')
+					)
+						.map(tag => ({ tag, level: +tag.tagName[1] }))
+				)
+			);
+		},
+		progress_bar() {
+			function resize() {
+				$('#progress-bar').style.display =
+					window.innerHeight < document.body.scrollHeight
+						? ''
+						: 'none';
+				scroll();
+			}
+			function scroll() {
+				const scroll_len = document.body.scrollHeight - window.innerHeight;
+				$('#progress-bar').style.width = window.scrollY/scroll_len*100 + '%';
+			}
 
-	$('.toc').innerHTML = generateToc(headingNodes);
+			window.addEventListener('resize', resize);
+			document.addEventListener('scroll', scroll);
+			resize();
+		},
+		top_button() {
+			function update() {
+				const activated = !!window.scrollY;
+				$('#top-button').style.opacity = 0.5*activated;
+				$('#top-button').style.pointerEvents = activated
+					? ''
+					: 'none';
+			}
+
+			window.addEventListener('resize', update);
+			document.addEventListener('scroll', update);
+			update();
+		}
+	},
+	enabled = [];
+
+window.require = (...args) => {
+	for(const x of args)
+		if(x in features)
+			enabled.push(features[x]);
+};
+window.require_all = () => {
+	window.require(...Object.keys(features).filter(x => x !== 'table_of_contents_new'));
+};
+window.require_all_new = () => {
+	window.require(...Object.keys(features).filter(x => x !== 'table_of_contents'));
 };
 
-fnlist.progressBar = function() {
-	window.addEventListener('resize', progressBarResize);
-	document.addEventListener('scroll', progressBarScroll);
-	progressBarResize();
-};
+document.addEventListener('DOMContentLoaded', () =>
+	enabled.forEach(x =>
+		x()
+	)
+);
 
-fnlist.topButton = function() {
-	window.addEventListener('resize', topButtonUpdate);
-	document.addEventListener('scroll', topButtonUpdate);
-	topButtonUpdate();
-};
-
-function progressBarResize() {
-	$('#progress-bar').style.display = innerHeight < document.body.scrollHeight ? '' : 'none';
-	progressBarScroll();
 }
-
-function progressBarScroll() {
-	$('#progress-bar').style.width = scrollY/(document.body.scrollHeight - innerHeight)*100 + '%';
-}
-
-function topButtonUpdate() {
-	var activated = !!scrollY;
-	$('#top-button').style.opacity = activated ? 0.5 : 0;
-	$('#top-button').style.pointerEvents = activated ? '' : 'none';
-}
-
-window.require = function() {
-	for(var i in arguments)
-		if(arguments[i] in fnlist)
-			required.push(fnlist[arguments[i]]);
-};
-window.requireAll = function() {
-	for(var i in fnlist)
-		required.push(fnlist[i]);
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-	for(var i in required)
-		required[i]();
-});
-
-})();
